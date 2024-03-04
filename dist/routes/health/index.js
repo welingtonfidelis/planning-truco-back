a9c3f845bb8c6e55fae6f892e4705d37ca001388
Welington Fidelis <welingtonfidelis@gmail.com>
2024-03-04 13:37:07 -0300

    Delete dist content (used only in deploy)

--- dist/routes/health/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.healthRouter = void 0;
-const express_1 = require("express");
-const health_1 = require("../../controllers/health");
-const healthRouter = (0, express_1.Router)();
-exports.healthRouter = healthRouter;
-healthRouter.get("/health", health_1.healthController.healthCheck);
-//# sourceMappingURL=index.js.map
--- dist/shared/const/jokerCardValue.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JokerCardValue = void 0;
-exports.JokerCardValue = [-1, 1000];
-//# sourceMappingURL=jokerCardValue.js.map
