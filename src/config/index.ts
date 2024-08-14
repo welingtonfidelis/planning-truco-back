a122cd31d94581f5b4427f7b39587ed5f07ffb15
welington fidelis <welingtonfidelis@gmail.com>
2024-08-14 08:38:51 -0300

    Create https options;

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,8 @@ dotenv.config();
 const config = {
   PORT: process.env.PORT,
   CORS_DOMAINS: process.env.CORS_DOMAINS as string,
+  CERTIFICATE_KEY: process.env.CERTIFICATE_KEY as string,
+  CERTIFICATE_CERT: process.env.CERTIFICATE_CERT as string,
 };
 
 export { config };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import SocketIo from "socket.io";
 import express from "express";
 import cors from "cors";
+import https from "https";
+import fs from "fs";
 import "express-async-errors";
 
 import { router } from "./route";
 import { config } from "./config";
 import { socketListener } from './socket'
 
-const { PORT, CORS_DOMAINS } = config;
+const { PORT, CORS_DOMAINS, CERTIFICATE_CERT, CERTIFICATE_KEY } = config;
 
 const corsDomain = CORS_DOMAINS.split(",").map((item) => item.trim());
 const corsOptions = {
@@ -21,9 +23,20 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use(router);
 
-const expressServer = app.listen(PORT, function () {
+// PROD
+const options = {
+  key: fs.readFileSync(CERTIFICATE_KEY),
+  cert: fs.readFileSync(CERTIFICATE_CERT),
+};
+
+const expressServer = https.createServer(options, app).listen(PORT, () => {
   console.log(`Server running in ${PORT}\n`);
 });
+
+// LOCAL
+// const expressServer = app.listen(PORT, function () {
+//   console.log(`Server running in ${PORT}\n`);
+// });
 
 const socketServer = new SocketIo.Server(expressServer, {
   cors: { origin: corsDomain },
